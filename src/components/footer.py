753458e2f8e3559c9f817a7def9e0a65d7e5e0a2
pawan7323 <pawangatla1111@gmail.com>
2026-06-19 14:31:15 +0530

    Add files via upload

---
+++ src/components/footer.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown("""
+    <div style="margin-top:2rem; display:flex; justify-content:center;">
+        <p style="font-weight:bold; color:black;">
+            Created with ❤️ by Pawan
+        </p>
+    </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+
+    st.markdown("""
+    <div style="margin-top:2rem; display:flex; justify-content:center;">
+        <p style="font-weight:bold; color:black;">
+            Created with ❤️ by Pawan
+        </p>
+    </div>
+    """, unsafe_allow_html=True)
